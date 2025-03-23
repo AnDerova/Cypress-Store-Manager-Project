932b8fa7a2b72a027d5ca49fbe5e1c6b25ca83bb
Anastasiia Derova <dyorovanastya@gmail.com>
2025-03-23 18:32:40 -0500

    Add Cypress tests for user registration with various invalid email and password scenarios

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -7,26 +7,27 @@ const registrationPage = new RegistrationPage();
 const loginPage = new LoginPage();
 
 describe("Registration Page Tests", () => {
+  let validUserName, validEmail, validPassword;
+
   beforeEach(() => {
     cy.visit("http://localhost:8000/register/");
+
+    validUserName = faker.person.firstName();
+    validEmail = faker.internet.email().toLowerCase();
+    validPassword = faker.internet.password({
+      length: 12,
+      pattern: /[A-Za-z0-9!@#$%^&*]/,
+    });
   });
 
   const nameLengths = [3, 7, 50];
-  const userName = faker.lorem.word(length);
-  const shortUserName = "ab";
-  const longUserName = "a".repeat(51);
-  const password = faker.internet.password({
-    length: 12,
-    pattern: /[A-Za-z0-9!@#$%^&*]/,
-  });
 
   nameLengths.forEach((length) => {
     it(`Registration with valid data - a name of ${length} characters`, () => {
-      const email = faker.internet.email().toLowerCase();
       registrationPage
-        .typeUserName(userName)
-        .typeEmail(email)
-        .typePassword(password)
+        .typeUserName(validUserName)
+        .typeEmail(validEmail)
+        .typePassword(validPassword)
         .clickRegisterButton();
 
       cy.url().should("include", "/login");
@@ -45,75 +46,158 @@ describe("Registration Page Tests", () => {
   });
 
   it("Display an error message when registering without entering a username", () => {
-    const email = faker.internet.email().toLowerCase();
     registrationPage
       .verifyRegistrationForm()
-      .typeEmail(email)
-      .typePassword(password)
+      .typeEmail(validUserName)
+      .typePassword(validPassword)
       .clickRegisterButton();
-    registrationPage.getErrorMessage().should('be.visible');
+    registrationPage.getErrorMessage().should("be.visible");
   });
 
   it("Display an error message when registering without entering a email", () => {
     registrationPage
       .verifyRegistrationForm()
-      .typeUserName(userName)
-      .typePassword(password)
+      .typeUserName(validUserName)
+      .typePassword(validPassword)
       .clickRegisterButton();
-    registrationPage.getErrorMessage().should('be.visible');
+    registrationPage.getErrorMessage().should("be.visible");
   });
 
   it("Display an error message when registering without entering a password", () => {
-    const email = faker.internet.email().toLowerCase();
     registrationPage
       .verifyRegistrationForm()
-      .typeUserName(userName)
-      .typeEmail(email)
+      .typeUserName(validUserName)
+      .typeEmail(validEmail)
       .clickRegisterButton();
-    registrationPage.getErrorMessage().should('be.visible');
+    registrationPage.getErrorMessage().should("be.visible");
   });
 
   it("Display an error message when registering with a username consisting of spaces", () => {
-    const email = faker.internet.email().toLowerCase();
     registrationPage
       .verifyRegistrationForm()
       .typeUserName(" ".repeat(10))
-      .typeEmail(email)
-      .typePassword(password)
+      .typeEmail(validEmail)
+      .typePassword(validPassword)
       .clickRegisterButton();
-    registrationPage.getErrorMessage().should('be.visible');
+    registrationPage.getErrorMessage().should("be.visible");
   });
 
   it("Display an error message when registering with a password consisting of spaces", () => {
-    const email = faker.internet.email().toLowerCase();
     registrationPage
       .verifyRegistrationForm()
-      .typeUserName(userName)
-      .typeEmail(email)
+      .typeUserName(validUserName)
+      .typeEmail(validEmail)
       .typePassword(" ".repeat(10))
       .clickRegisterButton();
-    registrationPage.getErrorMessage().should('be.visible');
+    registrationPage.getErrorMessage().should("be.visible");
   });
 
   it("Display an error message when registering with a 2-character username", () => {
-    const email = faker.internet.email().toLowerCase();
+    const shortUserName = "ab";
     registrationPage
       .verifyRegistrationForm()
       .typeUserName(shortUserName)
-      .typeEmail(email)
-      .typePassword(password)
+      .typeEmail(validEmail)
+      .typePassword(validPassword)
       .clickRegisterButton();
-    registrationPage.getErrorMessage().should('be.visible');
+    registrationPage.getErrorMessage().should("be.visible");
   });
 
-  it.only("Display an error message when registering with a 51-character username", () => {
-    const email = faker.internet.email().toLowerCase();
+  it("Display an error message when registering with a 51-character username", () => {
+    const longUserName = "a".repeat(51);
     registrationPage
       .verifyRegistrationForm()
       .typeUserName(longUserName)
-      .typeEmail(email)
-      .typePassword(password)
+      .typeEmail(validEmail)
+      .typePassword(validPassword)
       .clickRegisterButton();
-    registrationPage.getErrorMessage().should('be.visible');
+    registrationPage.getErrorMessage().should("be.visible");
+  });
+
+  it("Display an error message when email is missing '@'", () => {
+    const invalidEmail = faker.internet.email().replace("@", "");
+    registrationPage
+      .verifyRegistrationForm()
+      .typeUserName(validUserName)
+      .typeEmail(invalidEmail)
+      .typePassword(validPassword)
+      .clickRegisterButton();
+    registrationPage.getErrorMessage().should("be.visible");
+  });
+
+  it("Display an error message when email is missing '@'", () => {
+    const invalidEmail = faker.internet.email().replace("@", "@@@");
+    registrationPage
+      .verifyRegistrationForm()
+      .typeUserName(validUserName)
+      .typeEmail(invalidEmail)
+      .typePassword(validPassword)
+      .clickRegisterButton();
+    registrationPage.getErrorMessage().should("be.visible");
+  });
+
+  it("Display an error message when email is missing domain", () => {
+    const invalidEmail = faker.internet.email().split("@")[0] + "@";
+    registrationPage
+      .verifyRegistrationForm()
+      .typeUserName(validUserName)
+      .typeEmail(invalidEmail)
+      .typePassword(validPassword)
+      .clickRegisterButton();
+    registrationPage.getErrorMessage().should("be.visible");
+  });
+
+  it("Display an error message when email is missing domain", () => {
+    const invalidEmail = faker.internet.email().replace(/\.(?=\w+$)/, "");
+    registrationPage
+      .verifyRegistrationForm()
+      .typeUserName(validUserName)
+      .typeEmail(invalidEmail)
+      .typePassword(validPassword)
+      .clickRegisterButton();
+    registrationPage.getErrorMessage().should("be.visible");
+  });
+
+  it("Should display an error message when registering with an email containing spaces", () => {
+    const invalidEmail = faker.internet.email().replace("@", " @");
+    registrationPage
+      .verifyRegistrationForm()
+      .typeUserName(validUserName)
+      .typeEmail(invalidEmail)
+      .typePassword(validPassword)
+      .clickRegisterButton();
+    registrationPage.getErrorMessage().should("be.visible");
+  });
+
+  it("Should display an error message when registering with an already registered email", () => {
+    const registeredEmail = faker.internet.email();
+    registrationPage
+      .verifyRegistrationForm()
+      .typeUserName(validUserName)
+      .typeEmail(registeredEmail)
+      .typePassword(validPassword)
+      .clickRegisterButton();
+    cy.url().should("include", "/login");
+    cy.visit("http://localhost:8000/register/");
+
+    registrationPage
+      .verifyRegistrationForm()
+      .typeUserName(validUserName)
+      .typeEmail(registeredEmail)
+      .typePassword(validPassword)
+      .clickRegisterButton();
+
+    registrationPage.getErrorMessage().should("be.visible");
+  });
+
+  it("Should display an error message when registering with a short password (less than 8 characters", () => {
+    const shortPassword = faker.internet.password({ length: 5 });
+    registrationPage
+      .verifyRegistrationForm()
+      .typeUserName(validUserName)
+      .typeEmail(validEmail)
+      .typePassword(shortPassword)
+      .clickRegisterButton();
+    registrationPage.getErrorMessage().should("be.visible");
   });
 });
